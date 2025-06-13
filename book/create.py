5871bbe067fbd4cc0fb5c1be8cd8bb2b12454e8a
shadow-light <noreply@gracious.tech>
2025-06-13 14:26:11 +1000

    .

--- book/create.py
+++ book/create.py
@@ -1,12 +1,13 @@
 
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 
 def create():
 
     # Render HTML to PDF
     weasy_html = HTML(filename='shared/book.html')
-    weasy_doc = weasy_html.render()
+    weasy_css = CSS(filename='shared/book.css')
+    weasy_doc = weasy_html.render(stylesheets=[weasy_css])
     num_pages = len(weasy_doc.pages)
     weasy_doc.write_pdf('shared/book.pdf')
 
