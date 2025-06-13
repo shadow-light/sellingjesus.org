067211008043afa3d7b2916d2aabb088ea6a7069
shadow-light <noreply@gracious.tech>
2025-06-13 11:39:57 +1000

    book styles

--- src/book.vue
+++ src/book.vue
@@ -4,8 +4,15 @@
 h1 Abolish the Jesus Trade
 p Preface...
 
+h2 Contents
+template(v-for='section of toc')
+    h3 {{ section.title }}
+    template(v-for='chapter of section.chapters')
+        h4.toc(@click='goto(chapter.id)') {{ chapter.title }}
+
 div(class='profiles_html' v-html='profiles_html')
 
+div.convos
 div(class="titles")
     h2(id="convo-general") 2. Conversations about Selling Jesus
     div(class="author") Andrew Case
@@ -27,11 +34,21 @@ div(class='articles_html' v-html='articles_html')
 
 <script lang='ts' setup>
 
+import {useShadowRoot} from 'vue'
+
 import InstantMessages from './_comp/InstantMessages.vue'
 import {data as articles_data} from './book_articles.data'
 import {data as profiles_data} from './book_profiles.data'
 import convo_general from './learn/conversations_processed.json'
 import convo_corinthians from './learn/corinthians_processed.json'
+
+import styles from './book.sass?inline'
+
+
+// Need to manually inject book styles into shadow DOM
+defineOptions({
+    styles: [styles],
+})
 
 
 // Util to demote all headings in HTML
@@ -57,8 +74,8 @@ const sections:Record<string, string[]> = {
     "History": ['simony'],
     "Theology": ['defining-ministry', 'commerce-condemned', 'biblical-funding', 'colabor', 'sincerity', 'buying', 'judas'],
     "Specific Passages": ['selling-truth', 'temple-cleansing', '1cor9', '1cor9-authority', 'commercializing-gods-word'],
-    "Application": ['should-preachers-be-paid', 'paying-pastors', 'covering-costs', 'biblical-counseling', 'counseling-fees', 'ads', 'blood-money', 'pragmatism'],
-    "Licensing & Copyright": ['copyright-jesus-command-to-freely-give', 'copyright-and-the-bible', 'letting-go', 'abuse', 'copyright-hijacking', 'sharealike'],
+    "Application": ['should-preachers-be-paid', 'paying-pastors', 'covering-costs', 'biblical-counseling', 'ads', 'blood-money', 'pragmatism'],
+    "Licensing & Copyright": ['copyright-jesus-command-to-freely-give', 'copyright-and-the-bible', 'abuse', 'copyright-hijacking', 'sharealike'],
     "Specific Ministries": ['bible-publishers', 'worship-tax', 'acbc', 'kjv'],
 }
 
@@ -70,6 +87,9 @@ const additional = []
 for (const id in articles){
     if (!articles[id].frontmatter.category){
         continue  // No category is for April fools etc
+    }
+    if (articles[id].frontmatter.license){
+        continue  // Exclude anything with a license (not public domain)
     }
     if (!all_hardcoded.includes(id)){
         additional.push(id)
@@ -85,7 +105,7 @@ let articles_html = ''
 let ch = 1 + 3
 for (const section in sections){
 
-    articles_html += `<h1 id="${section}">${section}</h1>`
+    articles_html += `<h1>${section}</h1>`
 
     for (const article_id of sections[section]){
         const article = articles[article_id]
@@ -94,7 +114,7 @@ for (const section in sections){
         articles_html += '<div class="titles">'
         const title = article.frontmatter.title_h1 || article.frontmatter.title
         const subtitle = article.frontmatter.title_h2
-        articles_html += `<h2 id="${article_id}">${ch}. ${title}</h2>`
+        articles_html += `<h2 id="chapter-${article_id}">${ch}. ${title}</h2>`
         if (subtitle){
             articles_html += `<div class="subtitle">${subtitle}</div>`
         }
@@ -119,40 +139,3 @@ const profiles_html = demote_headings(profiles_data[0].html)
 
 
 </script>
-
-
-<style lang='sass'>
-
-._book
-    font-family: 'Noto Serif Hebrew', 'Noto Serif Greek', 'Crimson Pro', serif
-    font-size: 20px
-    line-height: 32px
-
-    td, th
-        font-size: 0.8em
-
-    a
-        color: inherit
-        text-decoration: none
-
-    blockquote > p
-        font-size: 0.9em
-
-    h1
-        break-before: page
-
-    .titles
-        break-before: page
-        text-align: center !important
-
-        .author
-            font-style: italic
-
-    .player
-        display: none
-
-    .profiles_html
-        img
-            display: none
-
-</style>
