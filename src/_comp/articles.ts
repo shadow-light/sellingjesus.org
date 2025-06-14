8bf958914d6e291ea9033d91a5a99fe29e9bb61b
shadow-light <noreply@gracious.tech>
2025-06-14 15:35:58 +1000

    .

---
+++ src/_comp/articles.ts
@@ -0,0 +1,55 @@
+
+export const articles_by_category:Record<string, string[]> = {
+    "Freely Giving": [
+        'freely-give',
+        'freely-give-today',
+        'scope',
+    ],
+    "Supporting Ministry": [
+        'defining-ministry',
+        'biblical-funding',
+        'colabor',
+    ],
+    "Selling Ministry": [
+        'commerce-condemned',
+        'sincerity',
+        'buying',
+        'judas',
+        'prostitutes-wages',
+    ],
+    "Specific Passages": [
+        'selling-truth',
+        'temple-cleansing',
+        '1cor9',
+        '1cor9-authority',
+        'commercializing-gods-word',
+    ],
+    "History": [
+        'simony',
+    ],
+    "Application": [
+        'should-preachers-be-paid',
+        'paying-pastors',
+        'covering-costs',
+        'biblical-counseling',
+        'ads',
+        'blood-money',
+        'pragmatism',
+    ],
+    "Copyright & Licensing": [
+        'copyright-jesus-command-to-freely-give',
+        'copyright-and-the-bible',
+        'abuse',
+        'copyright-hijacking',
+        'sharealike',
+    ],
+    "Contemporary Commerce": [
+        'bible-publishers',
+        'worship-tax',
+        'acbc',
+        'kjv',
+    ],
+}
+
+
+export const article_ids = Object.values(articles_by_category).flat()
