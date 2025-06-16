c7771be6f8cc91ddc92b59859b861f6c02f6a008
shadow-light <noreply@gracious.tech>
2025-06-16 11:34:40 +1000

    rm sep footnote plugin

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,6 @@
 import path from 'path'
 
 import MarkdownPluginFootnote from 'markdown-it-footnote'
-import MarkdownPluginFootnoteBook from './plugin_book_footnotes.mjs'
 import {defineConfig} from 'vitepress'
 
 import settings from '../settings.json'
@@ -76,12 +75,7 @@ export default defineConfig({
     markdown: {
         config: (md) => {
             md.options.typographer = true
-            // Book has different rendering of footnotes
-            if (process.env.BUILD_BOOK){
-                md.use(MarkdownPluginFootnoteBook)
-            } else {
-                md.use(MarkdownPluginFootnote)
-            }
+            md.use(MarkdownPluginFootnote)
         },
     },
     themeConfig: {
