d264f12b903d40189eb13c044e4fd99f47c0b0ce
shadow-light <noreply@gracious.tech>
2025-07-08 00:36:56 +1000

    .

--- book/create.py
+++ book/create.py
@@ -1,29 +1,25 @@
 
 import subprocess
-from pathlib import Path
 
 from weasyprint import HTML, CSS
 from weasyprint.text.fonts import FontConfiguration
 
 
-def create():
-    # Define paths
-    file_html = 'shared/dist/book.html'
-    file_css = 'shared/dist/book.css'
-
-    # Render HTML to PDF
+def create_pdf():
     font_config = FontConfiguration()
-    weasy_html = HTML(filename=file_html)
-    weasy_css = CSS(filename=file_css, font_config=font_config)
+    weasy_html = HTML(filename='shared/dist_pdf/book.html')
+    weasy_css = CSS(filename='shared/dist_pdf/book.css', font_config=font_config)
     weasy_doc = weasy_html.render(stylesheets=[weasy_css], font_config=font_config)
     weasy_doc.write_pdf('shared/book.pdf')
 
-    # Also build epub
+
+def create_epub():
     subprocess.run(
-        ['pandoc', 'book.html', '-o', '../book.epub', '--css', 'book.css', '--toc'],
-        cwd='shared/dist',
+        ['pandoc', 'book.html', '-o', '../book.epub', '--toc'],
+        cwd='shared/dist_epub',
         check=True,
     )
 
 
-create()
+create_pdf()
+create_epub()
