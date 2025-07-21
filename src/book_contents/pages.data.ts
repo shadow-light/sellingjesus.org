05f694353081118a4ac51e9ea2c2fc6da104d7d3
shadow-light <noreply@gracious.tech>
2025-07-21 10:05:47 +1000

    adjust paths

--- src/book_contents/pages.data.ts
+++ src/book_contents/pages.data.ts
@@ -3,6 +3,14 @@ import {createContentLoader} from 'vitepress'
 
 
 // WARN These will be sorted alphabetically regardless of input order
-export default createContentLoader(['book/conclusion.md', 'book/foreword.md', 'book/intro.md', 'learn/profiles.md'], {
-    render: true,
-})
+export default createContentLoader(
+    [
+        'book_contents/conclusion.md',
+        'book_contents/foreword.md',
+        'book_contents/intro.md',
+        'learn/profiles.md',
+    ],
+    {
+        render: true,
+    },
+)
