70f16716edbb4bbf4a1e0de2ccefe9855bd92a51
shadow-light <noreply@gracious.tech>
2025-07-22 11:42:39 +1000

    cleanup regions data

--- .bin/gen_regions.mjs
+++ .bin/gen_regions.mjs
@@ -6,8 +6,14 @@ import iso3166 from 'iso3166-2-db/i18n/dispute/UN/en.json' with {type: 'json'}
 
 
 const countries = Object.entries(iso3166).map(([code, data]) => {
-    const regions = data.regions.map(region => ({name: region.name, code: region.iso}))
+    let regions = data.regions.map(region => ({name: region.name, code: region.iso}))
     regions.sort((a, b) => a.name.localeCompare(b.name))
+
+    // Some countries have regions but no codes, which isn't useful
+    if (regions.some(r => !r.code)){
+        regions = []
+    }
+
     return {
         code,
         name: data.name,
